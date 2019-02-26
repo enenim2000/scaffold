159573f15f28ed9a6c32d0dcd7a42722f7c42ad2
Asukwo Enenim <Asukwo.Enenim@etranzact.com>
2019-02-26 09:20:59 +0100

    updates

--- src/main/java/com/enenim/scaffold/controller/TransactionController.java
+++ src/main/java/com/enenim/scaffold/controller/TransactionController.java
@@ -1,17 +1,29 @@
 package com.enenim.scaffold.controller;
 
 import com.enenim.scaffold.annotation.Get;
+import com.enenim.scaffold.annotation.Post;
 import com.enenim.scaffold.annotation.Role;
 import com.enenim.scaffold.constant.RoleConstant;
+import com.enenim.scaffold.dto.request.Request;
+import com.enenim.scaffold.dto.request.ServiceRequest;
+import com.enenim.scaffold.dto.request.TransactionRequest;
 import com.enenim.scaffold.dto.response.ModelResponse;
 import com.enenim.scaffold.dto.response.PageResponse;
 import com.enenim.scaffold.dto.response.Response;
+import com.enenim.scaffold.enums.Initiator;
+import com.enenim.scaffold.model.dao.Service;
+import com.enenim.scaffold.model.dao.ServiceForm;
 import com.enenim.scaffold.model.dao.Transaction;
 import com.enenim.scaffold.service.dao.ConsumerService;
 import com.enenim.scaffold.service.dao.TransactionService;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/transactions")
@@ -37,11 +49,31 @@ public class TransactionController {
         return new Response<>(new ModelResponse<>(transactionService.getTransaction(id)));
     }
 
-    /*@Post
+    @Post
     @Role({RoleConstant.STAFF, RoleConstant.CONSUMER})
-    public Response<ModelResponse<Branch>> createTransaction(@Valid @RequestBody Request<TransactionRequest> request){
-        //RequestUtil.setCommonRequestProperties(request);
+    public Response<ModelResponse<Transaction>> createTransaction(@Valid @RequestBody Request<TransactionRequest> request){
+        Transaction transaction = new Transaction();
+        TransactionRequest transactionRequest = request.getBody();
+        transaction.setBranch(null);
+        transaction.setCurrency(null);
+        transaction.setPaymentChannel(null);
+        transaction.setPaymentMethod(null);
+        transaction.setStaff(null);
+        transaction.setInitiator(Initiator.USER);
+        transaction.setConsumer(consumerService.getConsumer(transactionRequest.getConsumerId()));
 
-    }*/
+        Set<ServiceForm> serviceForms = new HashSet<>();
+        for(ServiceRequest serviceRequest : transactionRequest.getServices()){
+            ServiceForm serviceForm = new ServiceForm();
+            Service service = new Service();
+            service.setId(serviceRequest.getServiceId());
+            serviceForm.setPayload(serviceRequest.getConsumerForm());
+            serviceForm.setService(service);
+            serviceForms.add(serviceForm);
+        }
+        transaction.setServiceForms(serviceForms);
+
+        return new Response<>(new ModelResponse<>(transactionService.saveTransaction(transaction)));
+    }
 
 }
