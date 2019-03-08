b27da044358e264b63977431a1008913d7e5f335
Asukwo Enenim <Asukwo.Enenim@etranzact.com>
2019-03-08 13:12:41 +0100

    update

--- src/main/java/com/enenim/scaffold/ApplicationStartup.java
+++ src/main/java/com/enenim/scaffold/ApplicationStartup.java
@@ -4,20 +4,24 @@ import com.enenim.scaffold.service.dao.SettingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationStartup implements ApplicationRunner {
 
     private final SettingService settingService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public ApplicationStartup(SettingService settingService) {
+    public ApplicationStartup(SettingService settingService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.settingService = settingService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("\n Password: " + bCryptPasswordEncoder.encode("Password@123"));
         System.out.println("\n About to load database settings into memory \n");
         //SettingConfigUtil.updateSettings( settingService.getSettings() );
         System.out.println("\n Loaded database settings into memory \n");
